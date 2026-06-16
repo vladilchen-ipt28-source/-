082c9a24da630fd7b816121170567a61a7ed43a9
vladilchen-ipt28-source <vladilchen-ipt28@lll.kpi.ua>
2026-06-16 17:55:05 +0300

    Update preinstall.sh

--- lab5/preinstall.sh
+++ lab5/preinstall.sh
@@ -16,7 +16,7 @@ mkdir -p vhi_data
 for i in {1..25}
 do
    echo "Завантаження даних для області ID: $i..."
-   curl -s -o "vhi_data/vhi_id_${i}.csv" "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_page.php?country=UKR&province=${i}"
+   curl -s -o "vhi_data/vhi_id_${i}.csv" "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_page.php?country=UKR&PROVINCE=${i}"
 done
 
 echo "=== Підготовка середовища успішно завершена! ==="
