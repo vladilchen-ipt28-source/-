505a58e685416b9d507b808b2a9b1a7817a103c6
vladilchen-ipt28-source <vladilchen-ipt28@lll.kpi.ua>
2026-06-16 16:31:59 +0300

    Delete lab6/preinstall.sh

--- lab6/preinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "=== Встановлення залежностей для Лабораторної роботи 6 ==="
-sudo apt update
-sudo apt install libopencv-dev cmake g++ gcc -y
-echo "=== Встановлення завершено успішно ==="
